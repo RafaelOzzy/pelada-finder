552e3280eb8092ecd7c66ab21ac7ba9d53f0a4e1
Bruno Cachinho <bruno.cachinho@gmail.com>
2023-05-06 16:35:39 -0300

    Model Relation Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :games
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :phone_number, presence: true
 end

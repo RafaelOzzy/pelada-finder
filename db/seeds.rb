17331a0a77b10a9cbb888fdb90e5f3dc974d85af
Rafael Santos <rafael.ramos.s@gmail.com>
2023-05-09 15:02:30 -0300

    Sedding Users and Games
    Seeds comentados para caso precise rodar novor seeds n'ao duplique.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Game.create(neighborhood: "barra", date: Date.new(2023, 5, 6), time: Time.new(2023, 5, 6, 12, 0, 0), user: User.find(1))
+# Game.create(neighborhood: "barra", date: Date.new(2023, 5, 6), time: Time.new(2023, 5, 6, 12, 0, 0), user: User.find(1))
+
+require 'faker'
+
+# Seed de User comentado para não roodar + de 1x
+# Todas as senhas são 123123
+# 5.times do
+#   Faker::Config.locale = 'pt-BR'
+#   User.create(email: Faker::Internet.email, password: "123123",
+#               name: Faker::Name.name, phone_number: Faker::PhoneNumber.cell_phone)
+# end
+# puts 'User seed done'
+
+# Seed de User comentado para não roodar + de 1x
+# 5.times do
+#   Game.create(neighborhood: Faker::Address.city, date: Date.new(2023, 5, 6), time: Time.new(2023, 5, 6, 12, 0, 0), user: User.find(rand(1..5)))
+# end
+
+# puts 'Game seed done'

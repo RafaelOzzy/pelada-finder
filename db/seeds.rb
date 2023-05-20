6ca88849264818d2e6184d781317ac16ab78635a
Rafael Santos <rafael.ramos.s@gmail.com>
2023-05-20 17:02:18 -0300

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.destroy_all
 
 100.times do
   Faker::Config.locale = 'pt-BR'
-  User.create(email: Faker::Internet.email, password: "123123",
+  User.create!(email: Faker::Internet.email, password: "123123",
               name: Faker::Name.name, phone_number: Faker::PhoneNumber.cell_phone)
 end
 
@@ -26,7 +26,7 @@ end
 address = ["Rio de Janeiro, Rua Visconde de Piraja", "Rua da Passagem, Rio de janeiro", "Av das Americas, rio de janeiro", "Av atlantica, rio de janeiro", "Av Brasil, Rio de janeiro"]
 
 5.times do
-game = Game.create(address: address.sample, date: Date.new(2023, 5, 6), time: Time.new(2023, 5, 6, 12, 0, 0), user: User.all.sample)
+game = Game.create!(address: address.sample, date: Date.new(2023, 5, 6), time: Time.new(2023, 5, 6, 12, 0, 0), user: User.all.sample)
 10.times do
   GameParticipant.create(game: game, user: User.all.sample)
 end
